0c15a5bb80c53e72d3d3a8da7a92f91b381835dc
Maykel Arias Torres <hanibal920915@gmail.com>
2023-02-19 15:52:15 -0300

    Separed rools

--- README.md
+++
@@ -1,2 +0,0 @@
-# Django + React using Vite
-
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
-import React,{ useState } from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState<number>(0)
+  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
